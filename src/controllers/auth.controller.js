a066228e9cc8be89d3b104dbf24ba6e027f4d41b
Krishna <krishna9842740994@gmail.com>
2026-08-01 16:32:15 +0530

    login api fully-baked

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -60,6 +60,54 @@ return res.status(409).json(
 
 }
 
-module.exports={registerUser}
+
+
+async function loginUser(req,res){
+
+    const {username,email,password}=req.body;
+
+    const user=await userModel.findOne({
+        $or:[
+            {username},
+            {email}
+        ]
+    })
+
+    if(!user){
+        return res.status(401).json({
+            message:'Invalid credentials'
+        })
+    }
+
+    const isPasswordCorrect=await bcrypt.compare(password,user.password); // the user.password is coming from the databbase 
+
+    if(!isPasswordCorrect){
+        return res.status(401).json({
+            message:'Incorrect pasword'
+
+        })
+}
+const token=jwt.sign({
+
+    id:user._id,
+    role:user.role
+
+},process.env.JWT_SECRET)
+
+res.cookie('token',token);  //send the token to the client in the form of cookie
+
+res.status(200).json({
+    message:'User logged in successfully',
+    user:{
+        id:user._id,
+        username:user.username,
+        email:user.email,
+        role:user.role
+    }
+})
+
+}
+
+module.exports={registerUser,loginUser}
 
 
